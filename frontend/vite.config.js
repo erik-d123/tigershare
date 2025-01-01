548fa0a92012fa1b7017fc818186654a9e5c6493
Erik <ed1783@princeton.edu>
2024-12-31 21:30:09 -0500

    heroku

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Or '0.0.0.0' for explicit binding
+    port: 5173, // Optional: specify port, default is 5173
+  },
 })
